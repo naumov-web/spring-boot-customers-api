8cf56a1dac0b3e2e67a45b2454dc8d969830054b
Konstantin Naumov <naumov-web@yandex.ru>
2021-01-17 16:46:42 +0300

    Init project. Set dependencies in pom.xml

---
+++ src/main/java/ru/naumovweb/customersapi/CustomersApiApplication.java
@@ -0,0 +1,13 @@
+package ru.naumovweb.customersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersApiApplication.class, args);
+	}
+
+}
