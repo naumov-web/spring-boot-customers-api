cc1228c9ec30bec109265138854e3d27e4136e84
Konstantin Naumov <naumov-web@yandex.ru>
2021-01-19 00:32:43 +0300

    Create JWT token and add validation

--- src/main/java/ru/naumovweb/customersapi/dto/requests/RegisterDTO.java
+++ src/main/java/ru/naumovweb/customersapi/dto/requests/RegisterDTO.java
@@ -1,9 +1,18 @@
 package ru.naumovweb.customersapi.dto.requests;
 
-import lombok.Data;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Data
+@NoArgsConstructor
 public class RegisterDTO {
-    private final String email;
-    private final String password;
+
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
+    private final String email = null;
+
+    @NotNull(message = "Password cannot be null")
+    private final String password = null;
 }
